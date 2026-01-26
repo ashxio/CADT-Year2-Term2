1ccd06e1186626aa91bfe87cf89bc9a00184c5bb
ashxio <ashbookenhart@gmail.com>
2026-01-26 14:14:07 +0700

    coffeeshop

---
+++ OOP (Java)/coffeeshop/Menuitem.java
@@ -0,0 +1,11 @@
+public class MenuItem {
+    String name;
+    double price;
+    char size;
+
+    MenuItem(String name, double price, char size) {
+        this.name = name;
+        this.price = price;
+        this.size = size;
+    }
+}
---
+++ OOP (Java)/coffeeshop/ShopMain.java
@@ -0,0 +1,8 @@
+public class ShopMain {
+    public static void main (String[], args){
+        item1.name = "Latte";
+        item1.price = 2.50;
+        item1.size = "L"
+        
+    }
+}
